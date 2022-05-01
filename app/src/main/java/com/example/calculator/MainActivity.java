f4ebd4789e5970eb8f10fa10ffb336ba5a135370
Nosheen17 <hayat.mehreen17@gmail.com>
2022-05-01 10:05:37 +0500

    Declaring widgets

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,9 +2,17 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnClear, btnEqual, btnZero, btnOne, btnTwo,btnThree,btnFour,btnFive,btnSix,btnSeven,btnEight,btnNine;
+    Button btnMul,btnMod,btnDiv,btnAdd,btnSub;
+    TextView resultText;
+    int val1 = 0, val2 = 0;
+    boolean Add,Sub,Div,Mul,Mod;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
