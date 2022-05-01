36ae30ba11b47e9f4d83b74005bbeacaf121f2cc
Nosheen17 <hayat.mehreen17@gmail.com>
2022-05-01 10:07:54 +0500

    Calculator completed

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,18 +1,20 @@
 package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnClear, btnEqual, btnZero, btnOne, btnTwo,btnThree,btnFour,btnFive,btnSix,btnSeven,btnEight,btnNine;
     Button btnMul,btnMod,btnDiv,btnAdd,btnSub;
     TextView resultText;
     int val1 = 0, val2 = 0;
     boolean Add,Sub,Div,Mul,Mod;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,5 +37,151 @@ public class MainActivity extends AppCompatActivity {
         btnAdd = findViewById(R.id.btnAdd);
         btnSub = findViewById(R.id.btnSub);
         resultText = findViewById(R.id.resultTextView);
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resultText.setText("");
+            }
+        });
+        btnEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(resultText.getText().length() > 0){
+                    if (Add || Sub || Mul || Div || Mod) {
+                        val2 = Integer.parseInt(resultText.getText() + "");
+                    }
+
+                    if (Add) {
+                        resultText.setText(val1 + val2 + "");
+                        Add = false;
+                    }
+
+                    if (Sub) {
+
+                        resultText.setText(val1 - val2 + "");
+                        Sub = false;
+                    }
+
+                    if (Mul) {
+                        resultText.setText(val1 * val2 + "");
+                        Mul = false;
+                    }
+
+                    if (Div) {
+                        resultText.setText(val1 / val2 + "");
+                        Div = false;
+                    }
+                    if (Mod) {
+                        resultText.setText(val1 % val2 + "");
+                        Mod = false;
+                    }
+
+
+                }
+            }
+        });
+
+        btnMod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(resultText.getText().length() > 0){
+                    val1 = Integer.parseInt(resultText.getText() + "");
+                    Mod = true;
+                    resultText.setText("");
+                }
+            }
+        });
+
+        btnMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(resultText.getText().length() > 0){
+                    val1 = Integer.parseInt(resultText.getText() + "");
+                    Mul = true;
+                    resultText.setText("");
+                }
+            }
+        });
+
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(resultText.getText().length() > 0){
+                    val1 = Integer.parseInt(resultText.getText() + "");
+                    Div = true;
+                    resultText.setText("");
+                }
+            }
+        });
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(resultText.getText().length() > 0){
+                    val1 = Integer.parseInt(resultText.getText() + "");
+                    Add = true;
+                    resultText.setText("");
+                }
+            }
+        });
+
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(resultText.getText().length() > 0){
+                    val1 = Integer.parseInt(resultText.getText() + "");
+                    Sub = true;
+                    resultText.setText("");
+                }
+            }
+        });
+
+        btnZero.setOnClickListener(this);
+        btnOne.setOnClickListener(this);
+        btnTwo.setOnClickListener(this);
+        btnThree.setOnClickListener(this);
+        btnFour.setOnClickListener(this);
+        btnFive.setOnClickListener(this);
+        btnSix.setOnClickListener(this);
+        btnSeven.setOnClickListener(this);
+        btnEight.setOnClickListener(this);
+        btnNine.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.btnZero:
+                resultText.setText(new StringBuilder().append(resultText.getText()).append("0").toString());
+                break;
+            case R.id.btnOne:
+                resultText.setText(new StringBuilder().append(resultText.getText()).append("1").toString());
+                break;
+            case R.id.btnTwo:
+                resultText.setText(new StringBuilder().append(resultText.getText()).append("2").toString());
+                break;
+            case R.id.btnThree:
+                resultText.setText(new StringBuilder().append(resultText.getText()).append("3").toString());
+                break;
+            case R.id.btnFour:
+                resultText.setText(new StringBuilder().append(resultText.getText()).append("4").toString());
+                break;
+            case R.id.btnFive:
+                resultText.setText(new StringBuilder().append(resultText.getText()).append("5").toString());
+                break;
+            case R.id.btnSix:
+                resultText.setText(new StringBuilder().append(resultText.getText()).append("6").toString());
+                break;
+            case R.id.btnSeven:
+                resultText.setText(new StringBuilder().append(resultText.getText()).append("7").toString());
+                break;
+            case R.id.btnEight:
+                resultText.setText(new StringBuilder().append(resultText.getText()).append("8").toString());
+                break;
+            case R.id.btnNine:
+                resultText.setText(new StringBuilder().append(resultText.getText()).append("9").toString());
+                break;
+        }
     }
 }
